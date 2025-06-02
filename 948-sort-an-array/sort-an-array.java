025128be206d6226a8c83264266ac298f53678ad
Chandru <107676929+Chandru047@users.noreply.github.com>
2025-06-02 12:25:00 +0530

    Time: 66 ms (25.40%) | Memory: 55.3 MB (96.54%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,12 +1,13 @@
 class Solution {
-     void merge(int arr[] , int low ,int mid , int high)
+    void merge(int[] arr , int low , int mid , int high)
     {
-        ArrayList<Integer> temp = new ArrayList<>();
         int left = low ; 
-        int right = mid + 1 ; 
-        while(left <= mid && right <=high)
+        int right = mid + 1  ;
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        while(left <= mid && right <= high)
         {
-            if(arr[left] <= arr[right]) 
+            if(arr[left] <= arr[right])
             {
                 temp.add(arr[left]);
                 left++ ;
@@ -14,39 +15,36 @@ class Solution {
             else
             {
                 temp.add(arr[right]);
-                right++;
+                right++ ;
             }
-            
-            
         }
         while(left <= mid)
         {
             temp.add(arr[left]);
             left++ ;
         }
-        
         while(right <= high)
         {
             temp.add(arr[right]);
             right++ ;
         }
-        
-        for(int i = low ; i<=high ; i++)
+
+        for(int i = low ; i <= high ; i++)
         {
-            arr[i] = temp.get(i-low);
+            arr[i] = temp.get(i - low);
         }
     }
-    
-    void mS(int arr[] ,int low ,int high)
+    void mS(int[] arr , int low , int high)
     {
-        if(low >= high) return  ;
+        if(low == high) return  ;
         int mid = low + (high - low) / 2 ;
         mS(arr , low , mid);
-        mS(arr , mid + 1 , high);
-        merge(arr , low , mid , high);
+        mS(arr , mid+1 , high);
+        merge(arr,low,mid,high);
+
     }
     public int[] sortArray(int[] arr) {
-        mS(arr,0,arr.length-1);
-        return arr ;
+        mS(arr, 0, arr.length - 1);
+        return arr;
     }
 }

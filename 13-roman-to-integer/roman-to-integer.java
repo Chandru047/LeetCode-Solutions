ca49fa7b15daa4995e239f71649d673769e7e638
Chandru <107676929+Chandru047@users.noreply.github.com>
2025-07-03 21:58:54 +0530

    Time: 5 ms (53.57%) | Memory: 44.7 MB (68.26%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,36 +1,25 @@
-class Solution 
-{
-    public int romanToInt(String s) 
-    {
-        HashMap<Character,Integer> map = new HashMap<>();
+import java.util.*;
 
-        List<Character> keys = Arrays.asList('I', 'V', 'X' , 'L' , 'C' , 'D' , 'M');
-        List<Integer> values = Arrays.asList(1, 5, 10 , 50 , 100 , 500 , 1000);
+public class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);   
+        map.put('V', 5);
+        map.put('X', 10); 
+        map.put('L', 50);
+        map.put('C', 100); 
+        map.put('D', 500);
+        map.put('M', 1000);
 
-        for (int i = 0; i < keys.size(); i++) 
-        {
-         map.put(keys.get(i), values.get(i));
-        }
-
-        int sum = 0;
-        int prev = 0;
-
-        for (int i = s.length() - 1; i >= 0; i--) 
-        {
-            int curr = map.get(s.charAt(i));
-
-            if (curr < prev) 
-            {
-                sum -= curr;
-            } 
-            else 
-            {
-                sum += curr;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int val = map.get(s.charAt(i));
+            if (i < s.length() - 1 && val < map.get(s.charAt(i + 1))) {
+                result -= val;
+            } else {
+                result += val;
             }
-
-            prev = curr;
         }
-
-        return sum;
+        return result;
     }
 }

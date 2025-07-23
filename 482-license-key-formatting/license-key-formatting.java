43261bada379893885549eaa5f1bea1b3196ffa4
Chandru <107676929+Chandru047@users.noreply.github.com>
2025-07-23 12:12:34 +0530

    Time: 207 ms (8.37%) | Memory: 46.5 MB (6.25%) - LeetSync

--- 482-license-key-formatting/license-key-formatting.java
+++ 482-license-key-formatting/license-key-formatting.java
@@ -6,13 +6,12 @@ class Solution {
         for(String i:arr1){
             s+=i;
         }
-        char[] arr=s.toCharArray();
         String out="";
-        int n=arr.length;
+        int n=s.length();
         int count=0;
         for(int i=n-1;i>=0;i--){
            
-            out+=arr[i];
+            out+=s.charAt(i);
             count++;
             if(i!=0 && count == k){
                 out+="-";
